326ffc036c43e7a43b4c6ac16007bac6538b49ed
kwyzo <kawayiazo@gmail.com>
2025-04-24 22:35:24 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`服务器正在监听端口 ${port}`);
+});    
